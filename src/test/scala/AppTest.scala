d48f5e851c4b4fd3498c9f7fad0566ba8a68148a
Andy Ai <yanbo.ai@gmail.com>
2014-07-02 15:41:16 +0800

    Simple?

--- src/test/scala/AppTest.scala
+++ src/test/scala/AppTest.scala
@@ -1,6 +1,6 @@
 import org.scalatest._
 
-class AppTest extends FlatSpec with Matchers{
+class AppTest extends FlatSpec {
   "Default test" should "Print something" in {
     println("I'am a scala test")
   }
