cb182bd337ad64ed522aece6d0b0591387ad5dd3
Andy Ai <yanbo.ai@gmail.com>
2014-07-17 17:27:41 +0800

    Fixed word

--- src/test/scala/AppTest.scala
+++ src/test/scala/AppTest.scala
@@ -2,6 +2,6 @@ import org.scalatest._
 
 class AppTest extends FlatSpec with Matchers{
   "Default test" should "Print something" in {
-    println("I'am a scala test")
+    println("I'm a scala test")
   }
 }
